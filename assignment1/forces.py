9091b420179f04be6c5637a065befc4fcd7f3278
igjerapic24 <igjerapic@gmail.com>
2025-05-21 23:12:22 +0200

    changed to have correct COM caluclation and foreces. Also have uniform binning for large and small shifts

--- assignment1/forces.py
+++ assignment1/forces.py
@@ -1,9 +1,11 @@
 import os 
 import glob
+import re
 
 import MDAnalysis as mda
 import numpy as np
 import beadspring as bsa
+import matplotlib.pyplot as plt
 
 #def calculate_COM(atom_fragments):
 
@@ -13,50 +15,65 @@ def main():
     topology = 'c60_pair_with_bonds.data'
 
     traj_dir = "lmp_data"
-    traj_files = sorted(glob.glob(os.path.join(traj_dir, "dump.data*")))
+    traj_files = glob.glob(os.path.join(traj_dir, "dump.data*"))
+    traj_files = sorted(traj_files, key=lambda x : int(re.search(r'\d+', x).group()))
 
-    u = mda.Universe(topology, traj_files, format= "LAMMPSDUMP")
+    u = bsa.setup_universe(topology, traj_files)
 
     N_FRAMES = u.trajectory.n_frames
     N_ATOMS_A = u.atoms.fragments[0].n_atoms
     N_ATOMS_B = u.atoms.fragments[-1].n_atoms
- 
+     
     # iniitiializations       
-    c60a_positions = np.zeros((N_FRAMES, N_ATOMS_A, 3))
-    c60b_positions = np.zeros((((N_FRAMES, N_ATOMS_B, 3))))
+    c60a_COM = np.zeros((N_FRAMES, 3))
+    c60b_COM = np.zeros((N_FRAMES, 3))
 
-    c60a_forces = np.zeros_like(c60a_positions)
-    c60b_forces = np.zeros_like(c60b_positions)
+    c60a_forces = np.zeros((N_FRAMES, 3))
+    c60b_forces = np.zeros((N_FRAMES, 3))
 
     # Loop over the frames and load the positions and forces
-    for i,traj in enumerate(u.trajectory):    
+    for i, traj in enumerate(u.trajectory):    
         c60a  = u.atoms.fragments[0]
         c60b = u.atoms.fragments[-1]                      
+        c60a_forces[i] = sum(c60a.forces.copy())
+        c60b_forces[i] = sum(c60b.forces.copy())
         
-        c60a_positions[i] = c60a.positions.copy()
-        c60b_positions[i] = c60b.positions.copy()
+        c60a_COM[i] = c60a.center_of_mass()
+        c60b_COM[i] = c60b.center_of_mass()
 
-        c60a_forces[i] = c60a.forces.copy()
-        c60b_forces[i] = c60b.forces.copy()
-
-    # total forces on molecules 
-    c60a_force = np.sum(c60a_forces, axis=1)
-    c60b_force = np.sum(c60b_forces, axis=1)
-
-    # COM of molecules and connecting vector
-    c60a_COM = np.sum(c60a_positions, axis=1) # allowed as all atoms have same mass
-    c60b_COM = np.sum(c60b_positions, axis=1)
-
+    # vector pointing from c60a COM to c60b COM
     vect_connect = c60a_COM - c60b_COM
 
     # projection of forces on molecule a onto vector connecting of COMs
-    distances = np.einsum('ij,ij->i', vect_connect, vect_connect)
-    force_project = np.einsum('ij,ij->i', vect_connect, c60a_force ) / distances
-    
-    
-    #TODO binning force on c60a over distances 
+    distances = np.linalg.norm(vect_connect, axis=1)
+    force_project = np.einsum('ij,ij->i', vect_connect, c60b_forces ) / distances
 
 
+    stepsizes = [distances[i] - distances[i - 1] for i in range(1, len(distances))]
+    steps = np.arange(1, len(distances) + 1)
+
+    #TODO binning distances and then averageing force on c60a over distances 
+    bin_count_large = 20
+    bin_count_small = 10
+    center = len(distances)//2
+
+    bin_edges_large = np.linspace(min(distances[:center]), max(distances[:center]),
+                                  bin_count_large)
+    bin_centers_large = 0.5 * (bin_edges_large[:-1] + bin_edges_large[1:])
+
+
+    bin_edges_small = np.linspace(min(distances[center - 5:]), max(distances[center - 5:]), 
+                                  bin_count_small)
+    bin_centers_small = 0.5 * (bin_edges_small[:-1] + bin_edges_small[1:])
+
+    bin_centers = np.concatenate((bin_centers_large, bin_centers_small))
+    bin_edges = np.concatenate((bin_edges_large, bin_edges_small))
+
+    # averaging
+    avg_forces = np.zeros_like(bin_centers)
+    for i in range(len(bin_centers)):
+        mask = (distances >= bin_edges[i]) & (distances <= bin_edges[i + 1])
+        avg_forces[i] = np.average(force_project[mask])
     #TODO fitting with either moors or LJ potential
 
 
