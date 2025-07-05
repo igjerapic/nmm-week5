654b16648c135397f0a9c664ebb7053cc62cf20e
igjerapic24 <igjerapic@gmail.com>
2025-07-05 13:19:58 +0200

    changed comments and adjusted plotting thermos

--- assignment2/plotting_thermo.py
+++ assignment2/plotting_thermo.py
@@ -50,6 +50,7 @@ def main():
     plt.legend()
     plt.tight_layout()
     plt.savefig("temp_eta0.4.svg", dpi=300)
+    plt.clf()
     
     plt.figure(2)
     plt.xlabel(r"Time ($\tau$)")
@@ -57,6 +58,7 @@ def main():
     plt.legend(loc=(0.7, 0.6))
     plt.tight_layout()
     plt.savefig("press_eta0.4.svg", dpi=300)
+    plt.clf()
    
     
     plt.figure(3)
@@ -65,7 +67,89 @@ def main():
     plt.legend()
     plt.tight_layout()
     plt.savefig("totEng_eta0.4.svg", dpi=300)
+    plt.clf()
     #plt.show()
+    
 
+
+    epsilon = 2.0
+    etas = [0.4, 0.8]
+    for eta in etas:
+        file_name = f"epsilon{epsilon}_eta{eta}/" + data_file
+        df = {}
+        with open(file_name, 'rb') as f: 
+            df = pkl.load(f)
+        
+        plt.figure(1)
+        plt.plot(df["Time"], df["Temp"], label = f"$\\eta=${eta}")
+
+        plt.figure(2)
+        plt.plot(df["Time"], df["Press"], label=f"$\\eta=${eta}")
+
+        plt.figure(3)
+        plt.plot(df["Time"], df["TotEng"], label=f"$\\eta=${eta}")
+
+    plt.figure(1)
+    plt.xlabel(r"Time ($\tau$)")
+    plt.ylabel(r"Temp $(\epsilon / k_B )$")
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(f"temp_epsilon{epsilon}.svg", dpi=300)
+    
+    plt.figure(2)
+    plt.xlabel(r"Time ($\tau$)")
+    plt.ylabel(r"Pressure ($\epsilon / \sigma^3$)")
+    plt.legend(loc=(0.7, 0.6))
+    plt.tight_layout()
+    plt.savefig(f"press_epsilon{epsilon}.svg", dpi=300)
+   
+    
+    plt.figure(3)
+    plt.xlabel(r"Time ($\tau$)")
+    plt.ylabel(r"Total Energy ($\epsilon$)")
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(f"totEng_epsilon{epsilon}.svg", dpi=300)
+    plt.show()
+
+    epsilon = 1.0
+    etas = [0.58]
+    for eta in etas:
+        file_name = f"epsilon{epsilon}_eta{eta}/" + data_file
+        df = {}
+        with open(file_name, 'rb') as f: 
+            df = pkl.load(f)
+        
+        plt.figure(1)
+        plt.plot(df["Time"], df["Temp"], label = f"$\\eta=${eta}")
+
+        plt.figure(2)
+        plt.plot(df["Time"], df["Press"], label=f"$\\eta=${eta}")
+
+        plt.figure(3)
+        plt.plot(df["Time"], df["TotEng"], label=f"$\\eta=${eta}")
+
+    plt.figure(1)
+    plt.xlabel(r"Time ($\tau$)")
+    plt.ylabel(r"Temp $(\epsilon / k_B )$")
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(f"temp_epsilon{epsilon}_eta{eta}.png", dpi=300)
+    
+    plt.figure(2)
+    plt.xlabel(r"Time ($\tau$)")
+    plt.ylabel(r"Pressure ($\epsilon / \sigma^3$)")
+    plt.legend(loc=(0.7, 0.6))
+    plt.tight_layout()
+    plt.savefig(f"press_epsilon{epsilon}_eta{eta}.png", dpi=300)
+   
+    
+    plt.figure(3)
+    plt.xlabel(r"Time ($\tau$)")
+    plt.ylabel(r"Total Energy ($\epsilon$)")
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(f"totEng_epsilon{epsilon}_eta{eta}.png", dpi=300)
+    plt.show()
 if __name__=="__main__":
     main()
